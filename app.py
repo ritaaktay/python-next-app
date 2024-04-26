4b0e11c58a7ca8d7d6c0a045ffccd208bbed4e2c
ritaaktay <rita.aktay@vodaofone.com>
2024-04-26 15:11:45 +0100

    feat: Trying other Flask functionality

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def home():
-    query = flask.request.args 
+# This function takes a greeting from the url path
+# And a name from the query string to greet user
+@app.route('/greet/<greeting>', methods=['GET'])
+def greet(greeting):
     # Get /hello?key=value&name=Rita
     # query = {"key: "value", "name": "Rita"} 
-    return "Hello %s" %(query.get('name'))
+    query = flask.request.args 
+    res = flask.make_response("%s %s" %(greeting, query.get('name')))
+    res.headers['Content-type'] = 'text'
+    return res
+
+# This function returns the profile image from server
+@app.route('/profile', methods=['GET'])
+def profile():
+    return flask.send_file('public/profile.jpg')
